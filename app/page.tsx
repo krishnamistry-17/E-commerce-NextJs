b3c46e2a49e100924956191053633d8aff365f38
krishnamistry-17 <mistrykrishna17103@gmail.com>
2025-09-29 11:40:28 +0530

    added vercel setup

--- app/page.tsx
+++ app/page.tsx
@@ -12,9 +12,13 @@ const Page = () => {
   // const filteredProducts = products.filter(
   //   (product) => product.category === heading.title
   // );
+  const handleClick = () => {
+    console.log("API URL:", process.env.NEXT_PUBLIC_API_URL);
+  };
 
   return (
     <div className="max-w-[1640px] mx-auto xl:px-[143px] px-2 pt-[55px]">
+      <button onClick={handleClick}>click</button>
       <Categories />
       <PopularProduct />
       {/* <DailySells />
--- lib/axios.ts
+++ lib/axios.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_SITE_URL,
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000",
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
